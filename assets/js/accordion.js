3d61117aad6142b814295bcca4117511387d9982
uhooaha <uhooaha@kakao.com>
2025-02-20 17:17:38 +0900

    up v2

---
+++ assets/js/accordion.js
@@ -0,0 +1,28 @@
+const acco = document.querySelector(".acco-items");
+
+const openAcco = (accoItem) => {
+    const content = accoItem.querySelector(".acco-items__contents");
+    accoItem.classList.add("acco-active");
+    content.style.maxHeight = content.scrollHeight + "px";
+    
+};
+
+const closeAcco = (accoItem) => {
+    const content = accoItem.querySelector(".acco-items__contents");
+    accoItem.classList.remove("acco-active");
+    content.style.maxHeight = null;
+};
+
+acco.forEach(acco => {
+    const title = accoItem.querySelector(".acco-items__title");
+    const content = accoItem.querySelector("acco-items__contents");
+
+    title.onclick = () => {
+        if(content.style.maxHeight){
+            closeAcco(accoItem);
+        }else {
+            acco.forEach((accoItem) => closeAcco(accoItem));
+            openAcco(accoItem);
+        };
+    };
+});
